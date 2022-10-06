ea8e3052dbcdb4ac854735fcf3b0d156cc97f180
bhavin4397 <bhavinpatidar4397@gmail.com>
2022-10-06 17:39:52 +0530

    code

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -2,8 +2,21 @@ import { useContext } from 'react'
 import React from 'react'
 import { UserContext } from "./App"
 
-const UserProfile = () =>{
-
+const UserProfile = () => {
+    return (
+        <>
+            <h1><UserContext.Consumer>{
+                obj => {
+                    return obj.name;
+                }
+            }</UserContext.Consumer></h1>
+            <h1><UserContext.Consumer>{
+                obj => {
+                    return obj.age;
+                }
+            }</UserContext.Consumer></h1>
+        </>
+    )
 }
 
-export {UserProfile}
+export { UserProfile }
