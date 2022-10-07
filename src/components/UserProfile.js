855a7bbe7938bf3980689c639f39ab9bd9e35c84
bhavin4397 <bhavinpatidar4397@gmail.com>
2022-10-07 10:54:36 +0530

    code

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -7,8 +7,8 @@ const UserProfile = () =>{
     const {name,age}=useContext(UserContext)
     return(
         <>
-        <h1 id='name'>{name}</h1>
-        <h1 id='age'>{age}</h1>
+        <h1 id='name'>Name:- {name}</h1>
+        <h1 id='age'>Age:- {age}</h1>
         </>
     )
 }
